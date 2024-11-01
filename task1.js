6d66ef2117ce44cb6aebf291f10386ec3db27a70
rabby <rabby.65301@gmail.com>
2024-11-01 15:41:28 +0600

    solved task 1

---
+++ task1.js
@@ -0,0 +1,20 @@
+/* 
+1.Task: Array Filtering and Mapping
+Create an array of objects, each representing a person with properties like name, age, and gender. Write a function to filter out all females and then map the remaining people to an array of names. Print the final result.
+*/
+
+const peoples = [
+    { name: "Alice", age: 28, gender: "Female" },
+    { name: "Bob", age: 35, gender: "Male" },
+    { name: "Charlie", age: 22, gender: "Male" },
+    { name: "Diana", age: 30, gender: "Female" },
+    { name: "Ethan", age: 40, gender: "Male" },
+    { name: "Fiona", age: 25, gender: "Female" },
+    { name: "George", age: 27, gender: "Male" }
+];
+
+
+const maleNames = peoples.filter((people) => people.gender !== "Female").map((male) => (male.name))
+
+console.log(maleNames);
+/* Final result: [ 'Bob', 'Charlie', 'Ethan', 'George' ] */
